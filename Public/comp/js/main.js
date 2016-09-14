d1e4a1397918323dd54cf84dd41b8b95a977cc07
juntaoshui <595979939@qq.com>
2016-09-14 17:07:45 +0800

    goods

--- Public/comp/js/main.js
+++ Public/comp/js/main.js
@@ -7,23 +7,28 @@
 $(function() {
     //下拉列表整体操作
     var pulldown = function() {
+
         $(".pulldown").click(function() {
+            $(".pulldown-box").css("z-index",1);
+            $(this).parent().css("z-index",999);
+
+            $(".pulldown-list").hide();
+
             $(this).next().show().css({
-                'width':$(this).width(),
-                'top':$(this).height()
-        });
+                    'width':$(this).width(),
+                    'top':$(this).height()
+            });
 
             return false;
         });
 
         $(".pulldown-list li").click(function() {
+            alert("选 中的");
             var id = $(this).data("id");
             $(this).closest(".pulldown-box").find(".select-txt").html($(this).html());
             $(this).parent().hide();
             if (id) {
                 $(this).parent().next(":hidden").val(id);
-            } else {
-                $(this).parent().next(":hidden").val($(this).html());
             }
         });
 
@@ -45,7 +50,10 @@ $(function() {
   $.tabs(".editor-tabs",'.editor-tabs-con');
   $.tabs(".tabs",'.tabs-con');
    
-
+ //发货时间选择
+ $(".delivery-time a").click(function(){
+    $("input[name=sendtime]").val($(this).html());
+ });
 
 
 
